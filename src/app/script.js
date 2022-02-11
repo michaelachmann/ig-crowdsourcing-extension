4f9b4f9660a5046f1d3940ec9513328c03a329ac
Michael Achmann <michael@achmann.dev>
2022-02-11 13:21:28 +0100

    Sending messages accross tabs is working. Ready for Vue

--- src/app/script.js
+++ src/app/script.js
@@ -1,14 +1,16 @@
-console.log("Test!")
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension" + sender);
-      if (request.greeting === "hello")
-        sendResponse({farewell: "goodbye"});
-    }
-  );
+chrome.tabs.query({url: "https://www.instagram.com/*"}, results => {
+    chrome.tabs.sendMessage(results[0].id, {type: 'userIds'}, function(response) {
+        console.log(response)
+        })
+    })
+    
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+          console.log(sender.tab ?
+                      "from a content script:" + sender.tab.url :
+                      "from the extension" + sender);
+          if (request.greeting === "hello")
+            sendResponse({farewell: "goodbye"});
+        }
+      );   
 
-  chrome.runtime.sendMessage({url: '/src/inject/inject.html'}, function(response) {
-    console.log(response);
-})
